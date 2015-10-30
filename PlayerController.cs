fe277cf3088a4c35f7fa664f0b3deeee709e1584
binarydots <vazaraq@gmail.com>
2015-10-30 11:07:40 +1100

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	public Text scoreText;
+
+	private int score;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		score = 0;
+
+		updateScoreText ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log (other.gameObject.tag);
+
+		updateScoreText ();
+	}
+
+
+	void updateScoreText()
+	{
+		scoreText.text = "Score : " + score.ToString ();
+	}
+
+}
